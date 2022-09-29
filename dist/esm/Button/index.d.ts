7b1f65f1febe0b8b33463b2471a0c0c2fc94eaf4
杜路路 <xiaolu@caibeike.com>
2022-09-29 14:21:56 +0800

    组件开发初始化

--- dist/esm/Button/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonHTMLAttributes } from 'react';
-declare type btnType = 'permary' | 'ban';
-interface BtnProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'type'> {
-    type: btnType;
-    nativeType: ButtonHTMLAttributes<HTMLButtonElement>['type'];
-}
-declare const Button: (props: BtnProps) => JSX.Element;
-export default Button;
--- dist/esm/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Calendar } from "./Calendar";
-export { default as Button } from "./Button";
